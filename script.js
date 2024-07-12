4ff9290bea7d9b801b874db574c9f1dfe865ab54
DavidParedesV20 <145064481+DavidParedesV20@users.noreply.github.com>
2024-07-12 01:09:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ function calculate() {
     const numEat = parseInt(document.getElementById('numEat').value) || 0;
     const numDrink = parseInt(document.getElementById('numDrink').value) || 0;
 
-    let totalEatDrink = numEatDrink > 0 ? (costFood + costAlcohol) / numEatDrink : 0;
-    let totalEat = numEat > 0 ? costFood / numEat : 0;
-    let totalDrink = numDrink > 0 ? costAlcohol / numDrink : 0;
+    const totalNumEat = numEatDrink + numEat;
+    const totalNumDrink = numEatDrink + numDrink;
 
-    if (numEatDrink > 0 && numEat > 0 && numDrink > 0) {
-        totalEatDrink = (costFood + costAlcohol) / (numEatDrink + numEat + numDrink);
-        totalEat = costFood / (numEatDrink + numEat);
-        totalDrink = costAlcohol / (numEatDrink + numDrink);
-    }
+    let totalEatDrink = numEatDrink > 0 ? (costFood / totalNumEat) + (costAlcohol / totalNumDrink) : 0;
+    let totalEat = totalNumEat > 0 ? costFood / totalNumEat : 0;
+    let totalDrink = totalNumDrink > 0 ? costAlcohol / totalNumDrink : 0;
 
     document.getElementById('resultText').innerText = 
         Costo por persona que come y bebe alcohol: $${totalEatDrink.toFixed(2)}\n +
